65dd571a67d833635e8d98a311cf9baa01e3f944
TheharaH <thehara00@gmail.com>
2024-11-05 14:08:04 +0530

    ef

--- src/main/java/com/example/ood/SessionManager.java
+++ src/main/java/com/example/ood/SessionManager.java
@@ -1,4 +1,17 @@
 package com.example.ood;
 
 public class SessionManager {
+    private static User currentUser;
+
+    public static void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    public static User getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void clearSession() {
+        currentUser = null;
+    }
 }
