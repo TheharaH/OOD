b368fc1899b4b7019f4ba47684280f6c8fbf5c71
TheharaH <thehara00@gmail.com>
2024-11-23 11:09:45 +0530

    ef

--- src/main/java/com/example/ood/HelloApplication.java
+++ src/main/java/com/example/ood/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 820, 240);
-        stage.setTitle("Hello00000!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("InfoEdge");
         stage.setScene(scene);
         stage.show();
     }
